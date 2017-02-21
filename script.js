e025f59a2ce7e115420c64dbbab1f51d4feb3b9e
MSU Account <msuaccount@Marcuss-MacBook-Pro.local>
2017-02-21 16:52:16 -0500

    js intro drum sounds

---
+++ script.js
@@ -0,0 +1,9 @@
+function Sound1 () {
+    document.getElementById('play1').play();
+}
+function Sound2 () {
+    document.getElementById('play2').play();
+}
+function Sound3 () {
+    document.getElementById('play3').play();
+}
