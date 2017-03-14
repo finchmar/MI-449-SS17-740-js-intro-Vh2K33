f4d3cb26fe5019d7e532520de3b044494dab6368
MSU Account <msuaccount@Marcuss-MacBook-Pro.local>
2017-03-14 15:02:55 -0400

    redo of js-intro

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function Sound1 () {
-    document.getElementById('play1').play();
+var boomSound = new Audio('boom.wav');
+var kickSound = new Audio('kick.wav');
+var snareSound = new Audio('snare.wav');
+
+// Boom sound
+function Boom () {
+  boomSound.play()
 }
-function Sound2 () {
-    document.getElementById('play2').play();
+document.getElementById('boomButton').addEventListener('click', function () {
+  Boom()
+})
+
+// Kick sound
+function Kick () {
+  kickSound.play()
 }
-function Sound3 () {
-    document.getElementById('play3').play();
+document.getElementById('kickButton').addEventListener('click', function () {
+  Kick()
+})
+
+// Snare sound
+function Snare () {
+  snareSound.play()
 }
+document.getElementById('snareButton').addEventListener('click', function () {
+  Snare()
+})
